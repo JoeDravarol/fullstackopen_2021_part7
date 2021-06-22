14071b39f11ac538c230e09484edbe0cb51d9519
Joe <joedravarol@gmail.com>
2021-06-22 16:38:28 +0100

    Complete comments backend exercise step1 feat: Implement functionality to add comments to blog post

--- bloglist-backend/controllers/blogs.js
+++ bloglist-backend/controllers/blogs.js
@@ -1,10 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const Comment = require('../models/comment')
 const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
     .populate('user', { username: 1, name: 1 })
+    .populate('comments', { content: 1 })
   response.json(blogs)
 })
 
@@ -27,6 +29,22 @@ blogsRouter.post('/', userExtractor, async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const body = request.body
+  const blog = await Blog.findById(request.params.id)
+
+  const comment = new Comment({
+    content: body.comment,
+    blog: blog._id
+  })
+
+  const savedComment = await comment.save()
+  blog.comments = blog.comments.concat(savedComment)
+  const savedBlog = await blog.save()
+
+  response.status(201).json(savedBlog)
+})
+
 blogsRouter.delete('/:id', userExtractor, async (request,response) => {
   const blog = await Blog.findById(request.params.id)
   const user = request.user
@@ -42,14 +60,7 @@ blogsRouter.delete('/:id', userExtractor, async (request,response) => {
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
-
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  }
+  const blog = request.body
 
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
